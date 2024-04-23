0a2d4b536ac974bc63187e42b1d41b88e2aa5b60
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-04-23 12:55:28 +0900

    修正

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,10 +6,7 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {
-	delete sprite_;
-	delete model_;
-}
+GameScene::~GameScene() {}
 
 void GameScene::Initialize() {
 
@@ -17,69 +14,9 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 
-	//---------------------------------------------
-	//01_03の変更。サウンドやテクスチャの追加と起動。
-	//---------------------------------------------
-
-	//ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("sample.png");
-
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-
-	//サウンドデータの読み込み
-	soundDataHandle_ = audio_->LoadWave("fanfare.wav");
-	//音声再生
-	audio_->PlayWave(soundDataHandle_);
-
-	//音声再生をループ
-	voiceHandle_ = audio_->PlayWave(soundDataHandle_,true);
-
-	// 3Dモデルの生成
-	model_ = Model::Create();
-
-	// ワールドトランスフォームの初期化
-	worldTransform_.Initialize();
-	// ビュープロジェクションの初期化
-	viewProjection_.Initialize();
-
-	//ライン描画が参照するビュープロジェクションを指定する（アドレス渡し）
-	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
-	
-
 }
 
-void GameScene::Update() {
-	// スプライトの今の座標を取得
-	Vector2 position = sprite_->GetPosition();
-	// 座標を{2,1}移動
-	position.x += 2.0f;
-	position.y += 1.0f;
-	// 移動した座標をスプライトに反映
-	sprite_->SetPosition(position);
-
-	if (input_->TriggerKey(DIK_SPACE)) {
-		//音声停止
-		audio_->StopWave(voiceHandle_);
-	}
-
-
-#ifdef _DEBUG
-
-	//ウィンドウの作成
-	ImGui::Begin("Debug1");
-	//デバッグテキストの表示
-	ImGui::Text("Ozawa Yuto %d.%d.%d", 2005, 03, 13);
-	//float3入力ボックス
-	ImGui::InputFloat3("InputFloat3", inputFloat3);
-	//float3スライダー
-	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
-	//デモウィンドウの表示を有効化
-	ImGui::ShowDemoWindow();
-	ImGui::End();
-
-#endif
-
-}
+void GameScene::Update() {}
 
 void GameScene::Draw() {
 
@@ -108,15 +45,8 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
-	
-    //3Dモデル描画
-	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
-
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
-
-	//ラインを描画する{Vector3 始点座標},{Vector3 終点座標},{Vector4 RGBA指定}
-	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
 
 #pragma endregion
 
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -5,8 +5,6 @@
 #include "Input.h"
 #include "Model.h"
 #include "Sprite.h"
-#include "ViewProjection.h"
-#include "WorldTransform.h"
 
 /// <summary>
 /// ゲームシーン
@@ -43,25 +41,6 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
-	
-	//テクスチャハンドル
-	uint32_t textureHandle_ = 0;
-	//スプライト
-	Sprite* sprite_ = nullptr;
-	// 3Dモデル
-	Model* model_ = nullptr;
-	//サウンドデータハンドル
-	uint32_t soundDataHandle_ = 0;
-	//音声再生ハンドル
-	uint32_t voiceHandle_ = 0;
-
-	// ワールドトランスフォーム
-	WorldTransform worldTransform_;
-	// ビュープロジェクション
-	ViewProjection viewProjection_;
-
-	//ImGuiで値を入力する変数
-	float inputFloat3[3] = {0, 0, 0};
 	
 	/// <summary>
 	/// ゲームシーン用
