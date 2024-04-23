66690d397576566d93c704971937f38616f8f2ce
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-04-23 13:03:55 +0900

    Init

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -9,11 +9,9 @@ GameScene::GameScene() {}
 GameScene::~GameScene() {}
 
 void GameScene::Initialize() {
-
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
-
 }
 
 void GameScene::Update() {}
