67f0ca4ac3d220753d16323d2c927165d561b626
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-05-31 00:11:02 +0900

    セーブ

--- Skydome.cpp
+++ Skydome.cpp
@@ -1,10 +1,18 @@
 #include "Skydome.h"
+#include <cassert>
 
-void Skydome::Initialize() {}
-
-void Skydome::Update() {}
-
-void Skydome::Draw(ViewProjection& viewProjection) {
-	//3Dモデル描画
-	model_->Draw(worldTransform_, viewProjection);
+void Skydome::Initialize(Model* model, ViewProjection* viewProjection) {
+	// nullptrチェック
+	assert(model);
+	model_ = model;
+	viewProjection_ = viewProjection;
+	worldTransform_.Initialize();
+	worldTransform_.scale_ = {1.f, 1.f, 1.f};
 }
+
+void Skydome::Update() {
+	worldTransform_.matWorld_ = MakeAffineMatrix(worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+	worldTransform_.TransferMatrix();
+}
+
+void Skydome::Draw() { model_->Draw(worldTransform_, *viewProjection_); }
