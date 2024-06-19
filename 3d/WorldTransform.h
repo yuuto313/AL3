315d3832c62ea15234d5aae05beaaefc3fa0fe0d
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-06-19 12:03:10 +0900

    セーブ

--- 3d/WorldTransform.h
+++ 3d/WorldTransform.h
@@ -47,6 +47,19 @@ public:
 	/// </summary>
 	void TransferMatrix();
 	/// <summary>
+	/// アフィン変換行列
+	/// </summary>
+	void UpdateMatrix() {
+		// スケール、回転、平行移動を合成して行列を計算する
+		matWorld_ = MakeAffineMatrix(scale_, rotation_, translation_);
+		// 親があれば親のワールド行列を掛ける
+		if (parent_) {
+			matWorld_ *= parent_->matWorld_;
+		}
+		// 定数バッファに転送する
+		TransferMatrix();
+	};
+	/// <summary>
 	/// 定数バッファの取得
 	/// </summary>
 	/// <returns>定数バッファ</returns>
