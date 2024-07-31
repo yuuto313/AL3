e761bd18f5613f523d8a499b8fc0abd6929ddde2
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-07-31 12:27:59 +0900

    セーブ

--- 3d/WorldTransform.h
+++ 3d/WorldTransform.h
@@ -49,11 +49,18 @@ public:
 	/// </summary>
 	void TransferMatrix();
 	/// <summary>
-	/// 定数バッファの取得
+	/// アフィン変換行列
 	/// </summary>
-	/// <returns>定数バッファ</returns>
-	
-	void UpdateMatrix();
+	void UpdateMatrix() {
+		// スケール、回転、平行移動を合成して行列を計算する
+		matWorld_ = MakeAffineMatrix(scale_, rotation_, translation_);
+		// 親があれば親のワールド行列を掛ける
+		if (parent_) {
+			matWorld_ *= parent_->matWorld_;
+		}
+		// 定数バッファに転送する
+		TransferMatrix();
+	};
 
 	const Microsoft::WRL::ComPtr<ID3D12Resource>& GetConstBuffer() const { return constBuffer_; }
 
