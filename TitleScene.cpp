657fe6fb26ee351ee61cf453ce6d0e6b6a36ad06
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-07-31 21:28:54 +0900

    セーブ

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,65 +1,40 @@
 #include "TitleScene.h"
-#include <cassert>
 #include "Input.h"
-TitleScene::~TitleScene() { 
-	delete model_; 
-	delete fade_;
+#include <cassert>
+TitleScene::~TitleScene() {
+	delete titleModel_;
+	delete UIModel_;
 }
 
 void TitleScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 
-	//各種初期化
-	model_ = Model::CreateFromOBJ("title", true);
-	assert(model_);
-	worldTransform_.Initialize();
-	worldTransform_.rotation_ = {90.f, 0.0f, 0.0f};
+	// 各種初期化
+	titleModel_ = Model::CreateFromOBJ("title", true);
+	UIModel_ = Model::CreateFromOBJ("titleUI", true);
+
+	assert(titleModel_);
+	assert(UIModel_);
+
+	titleWorldTransform_.Initialize();
+	UIWorldTransform_.Initialize();
+
+	titleWorldTransform_.translation_ = {0.f, 10.0f, 0.0f};
+	titleWorldTransform_.rotation_ = {0.3f, 0.0f, 0.0f};
+
+	UIWorldTransform_.translation_ = {0.0f, -9.0f, 0.0f};
+
 	viewProjection_.Initialize();
-	fade_ = new Fade();
-	fade_->Initialize();
-	float duration = 2.0f;
-	fade_->Start(Fade::Status::FadeIn, duration);
 }
 
-void TitleScene::Update() { 
-
-	switch (phase_) {
-	case TitleScene::Phase::kFadeIn:
-
-		fade_->Update();
-
-		// フェードイン中にフェードが終わったらメインフェーズに切り替える
-		if (fade_->IsFinished()) {
-			phase_ = Phase::kMain;
-		}
-
-		break;
-	case TitleScene::Phase::kMain:
-
-		// スペースでフェードアウトを開始する
-		if (Input::GetInstance()->PushKey(DIK_SPACE)) {
-			phase_ = Phase::kFadeOut;
-		}
-
-		break;
-	case TitleScene::Phase::kFadeOut:
-
-		fade_->Update();
-
-		// フェードアウト中にフェードが終了したらタイトルシーンを終了する
-		if (fade_->IsFinished()) {
-			finished_ = true;
-		}
-
-		break;
-	default:
-		break;
+void TitleScene::Update() {
+	if (Input::GetInstance()->PushKey(DIK_SPACE)) {
+		finished_ = true;
 	}
 
-	fade_->Update();
-
-	worldTransform_.UpdateMatrix();
+	titleWorldTransform_.UpdateMatrix();
+	UIWorldTransform_.UpdateMatrix();
 }
 
 void TitleScene::Draw() {
@@ -72,34 +47,13 @@ void TitleScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	
-	//モデル描画
-	model_->Draw(worldTransform_, viewProjection_);
 
-	switch (phase_) {
-	case TitleScene::Phase::kFadeIn:
-
-		fade_->Draw(commandList);
-
-		break;
-	case TitleScene::Phase::kMain:
-		break;
-	case TitleScene::Phase::kFadeOut:
-
-		fade_->Draw(commandList);
-
-		break;
-	default:
-		break;
-	}
+	// モデル描画
+	titleModel_->Draw(titleWorldTransform_, viewProjection_);
+	UIModel_->Draw(UIWorldTransform_, viewProjection_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 
 #pragma endregion
-	
-}
-
-void TitleScene::ChangePhase() {
-
 }
