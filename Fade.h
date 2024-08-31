84ac8ffbf44001812aaaed6aa46e39e9d1f45207
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-09-01 06:56:57 +0900

    セーブ

---
+++ Fade.h
@@ -0,0 +1,22 @@
+#pragma once
+/// <summary>
+/// フェード
+/// </summary>
+class Fade {
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialzie();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+
+	void Draw();
+
+private:
+
+
+};
