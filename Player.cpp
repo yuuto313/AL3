c92b392de6349222ee6646a7235edb83fa4f3ac6
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-08-31 14:22:14 +0900

    セーブ

--- Player.cpp
+++ Player.cpp
@@ -188,7 +188,8 @@ void Player::BehaviorjumpUpdate() {
 
 void Player::BehaviorDashUpdate() {
 
-
+	//自キャラの向いてる方向に移動する処理
+	//DashMovement();
 
 	//ダッシュの時間<frame>
 	const uint32_t behaviorDashTime = 2;
@@ -400,7 +401,54 @@ void Player::Movement() {
 
 }
 
-void Player::DashMovement() {}
+void Player::DashMovement() {
+	// 速さ
+	const float speed = 0.7f;
+	XINPUT_STATE joyState;
+
+	Input::GetInstance()->GetJoystickState(0, joyState);
+
+	if ((float)joyState.Gamepad.sThumbLX != 0 || (float)joyState.Gamepad.sThumbLY != 0) {
+
+		// 移動量
+		velocity_ = {(float)joyState.Gamepad.sThumbLX / SHRT_MAX, 0.0f, (float)joyState.Gamepad.sThumbLY / SHRT_MAX};
+
+		// 移動処理
+		// 移動量に速さを反映
+		velocity_ = velocity_ * speed;
+
+		// カメラの回転角度を取得
+		Vector3 rotationAngle = {GetViewProjection()->rotation_.x, GetViewProjection()->rotation_.y, GetViewProjection()->rotation_.z};
+
+		////カメラの角度から回転行列を計算する
+		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotationAngle.x);
+		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotationAngle.y);
+		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotationAngle.z);
+		Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
+		// 移動ベクトルをカメラの座標だけ回転する
+		velocity_ = TransformNormal(velocity_, rotateXYZMatrix);
+
+		// 移動
+		worldTransform_.translation_ += velocity_;
+
+		//--------------------------------
+		// 移動方向に見た目を合わせる
+		//--------------------------------
+
+		// 目標角度の算出
+		worldTransformBody_.rotation_.y = std::atan2(velocity_.x, velocity_.z);
+
+	} else if (lockOn_ && lockOn_->ExistTarget()) {
+		// ロックオン座標
+		Vector3 lockOnPosition = lockOn_->GetTargetPosition();
+		// 追従対象からロックオン対象へのベクトル
+		Vector3 sub = lockOnPosition - worldTransform_.translation_;
+
+		// Y軸周り角度
+		worldTransform_.rotation_.y = std::atan2(sub.x, sub.z);
+	}
+}
 
 void Player::InitializeFloatingGimmick() { 
 	//浮遊ギミックに使う変数を初期化
