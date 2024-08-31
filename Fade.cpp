3f12fd62c750d84845c684f227f4b650883433b2
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-09-01 06:59:13 +0900

    セーブ

--- Fade.cpp
+++ Fade.cpp
@@ -1,1 +1,9 @@
 #include "Fade.h"
+
+void Fade::Initialzie() {
+	sprite_=
+}
+
+void Fade::Update() {}
+
+void Fade::Draw(ID3D12GraphicsCommandList* commandList) {}
--- Fade.h
+++ Fade.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "DirectXCommon.h"
+#include "Sprite.h"
 /// <summary>
 /// フェード
 /// </summary>
@@ -13,10 +15,12 @@ class Fade {
 	/// </summary>
 	void Update();
 
-
-	void Draw();
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="commandList"></param>
+	void Draw(ID3D12GraphicsCommandList* commandList);
 
 private:
-
-
+	Sprite* sprite_ = nullptr;
 };
